4d4f9cc90e60945e3b49d92ae1783fc9fa77cdbb
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-05-03 15:33:30 +0500

    load port from env

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         httpsOptions
     });
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 5000);
 }
 
 bootstrap();
